d41fc982b22b22263c73526c6952309c041dcafc
liendmch <mon823@naver.com>
2021-01-23 07:25:36 +0900

    chore: react helmet 및 gatsby alias 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,26 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+const path = require('path');
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
   plugins: [
     `gatsby-plugin-typescript`,
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          '@src': path.resolve(__dirname, './src/'),
+          '@components': path.resolve(__dirname, './src/components/'),
+          '@styles': path.resolve(__dirname, './src/styles/'),
+          '@static': path.resolve(__dirname, './src/static/'),
+          '@layouts': path.resolve(__dirname, './src/layouts/'),
+          '@pages': path.resolve(__dirname, './src/pages/'),
+        },
+        extensions: ['tsx', 'js', 'ts'],
+      },
+    },
   ],
 };
